62c8c84294d1fb6329b42d66c35a1dc2623f2747
emilmont <emilmont@gmail.com>
2013-07-27 16:43:40 +0100

    Parse the last question :-)

--- example.py
+++ example.py
@@ -3,4 +3,6 @@ from stat_parser.parser import Parser
 
 if __name__ == '__main__':
     parser = Parser()
-    print parser.parse("Who leads the star ship Enterprise in Star Trek ?")
+    
+    # http://www.thrivenotes.com/the-last-question/
+    print parser.parse("How can the net amount of entropy of the universe be massively decreased?")
