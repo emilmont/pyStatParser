54d78f932a383d466035d6d3fb7a0990635f6519
emilmont <emilmont@gmail.com>
2013-07-29 23:27:08 +0100

    speedup test with multiprocessing

--- test.py
+++ test.py
@@ -1,24 +1,65 @@
 from json import loads
 from time import time
+from multiprocessing import Process, JoinableQueue, cpu_count
+from threading import Thread
 
 from stat_parser.paths import TEST_DAT, TEST_KEY
 from stat_parser.parser import Parser
 from stat_parser.eval_parser import ParseEvaluator
 
 
-if __name__ == '__main__':
-    parser = Parser()
-    evaluator = ParseEvaluator()
+def test_process(parser, sentences, parsed):
+    while True:
+        key, sent = sentences.get()
+        tree = parser.norm_parse(sent)
+        parsed.put((key, tree))
+        sentences.task_done()
+
+
+class Evaluator(Thread):
+    def __init__(self, parsed):
+        Thread.__init__(self)
+        self.results = ParseEvaluator()
+        self.parsed = parsed
+        
+        self.daemon = True
+        self.start()
     
+    def run(self):
+        while True:
+            key, tree = self.parsed.get()
+            try:
+                self.results.check_trees(loads(key), tree)
+            except Exception, e:
+                print '\nparsed: {%s}' % (tree)
+                print 'key   : {%s}' % (key.strip())
+                print e
+            self.parsed.task_done()
+
+
+def test():
+    parser = Parser()
+    sentences = JoinableQueue()
+    parsed = JoinableQueue()
+    
+    # Parsers Processes consuming sentences
+    for _ in range(cpu_count()):
+        p = Process(target=test_process, args=(parser, sentences, parsed))
+        p.daemon = True
+        p.start()
+    
+    # Evaluator Thread consuming parsed trees
+    evaluator = Evaluator(parsed)
     start = time()
-    for key, dat in zip(open(TEST_KEY), open(TEST_DAT)):
-        tree = None
-        try:
-            tree = parser.norm_parse(dat)
-            evaluator.check_trees(loads(key), tree)
-        except Exception, e:
-            print '\nparsed: {%s}' % (tree)
-            print 'key   : {%s}' % (key.strip())
-            print e
-    evaluator.output()
+    for key, sent in zip(open(TEST_KEY), open(TEST_DAT)):
+        sentences.put((key, sent))
+    
+    # Wait completion
+    sentences.join()
+    parsed.join()
+    evaluator.results.output()
     print '\nCompleted in (%.2f)sec' % (time() - start)
+
+
+if __name__ == '__main__':
+    test()
