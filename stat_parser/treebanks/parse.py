4cb199b5b3e8349307fb35a86924460076169247
James <CrazyPython@users.noreply.github.com>
2016-03-12 17:48:56 -0500

    Fix typo 'vereb phrase' to 'verb' phrase

--- stat_parser/treebanks/parse.py
+++ stat_parser/treebanks/parse.py
@@ -21,7 +21,7 @@ TAGS = set((
     'QP',     # Quantifier Phrase (i.e. complex measure/amount phrase); used within NP.
     'RRC',    # Reduced Relative Clause. 
     'UCP',    # Unlike Coordinated Phrase. 
-    'VP',     # Vereb Phrase. 
+    'VP',     # Verb Phrase. 
     'WHADJP', # Wh-adjective Phrase. Adjectival phrase containing a wh-adverb, as in how hot.
     'WHADVP',  # Wh-adverb Phrase. Introduces a clause with an NP gap. May be null (containing the 0 complementizer) or lexical, containing a wh-adverb such as how or why.
     'WHNP',   # Wh-noun Phrase. Introduces a clause with an NP gap. May be null (containing the 0 complementizer) or lexical, containing some wh-word, e.g. who, which book, whose daughter, none of which, or how many leopards.
